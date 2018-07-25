1fec988e29cea597af1314573f1db2725bdb37f0
davidmyers6643 <davidmyers6643@gmail.com>
2018-07-25 10:35:09 -0500

    fix

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,26 +1,32 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int i = 5;
-//        boolean condition = true;
-        while (i <= 15) {
-            System.out.print(i);
-            System.out.print(" ");
-            i += 1;
-        }
-        int f = 0;
-        do {
-            System.out.println(i);
-            i += 2;
-        } while (i <= 100);
-
-
-        int g = 100;
-        do {
-            System.out.println(g);
-            g -= 5;
-        } while (g >= -10);
-
+//        int i = 5;
+////        boolean condition = true;
+//        while (i <= 15) {
+//            System.out.print(i);
+//            System.out.print(" ");
+//            i += 1;
+//        }
+//
+//
+//        int f = 0;
+//        do {
+//            System.out.println(f);
+//            f += 2;
+//        } while (f <= 100);
+//
+//
+//
+//
+//        int g = 100;
+//        do {
+//            System.out.println(g);
+//            g -= 5;
+//        } while (g >= -10);
+//
+//
+//
 
         long e = 2;
 
@@ -29,6 +35,16 @@ public class ControlFlowExercises {
             e *= e;
         } while (e <=65536);
 
-        for () {}
+        for (int d = 0; d <= 100; d+=1) {
+            if (d % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (d % 5 == 0){
+                System.out.println("Buzz");
+            } else if (d % 3 == 0){
+                System.out.println("Fizz");
+            }else {
+                System.out.println(d);
+            }
+        }
     }
 }
